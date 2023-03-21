35694783ca542edd8bbe7d93ec85cadbaa62a718
Mark Fisher <mafisher888@gmail.com>
2023-03-21 20:58:46 +0000

    Create playRound function

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
+
+function playRound(playerChoice, computerChoice) {
+  // Lowercase playerChoice and capitalize first letter to match computerChoice format
+  playerChoice = playerChoice.toLowerCase();
+  playerChoice = playerChoice.charAt(0).toUpperCase + playerChoice.slice(1);
+
+  if (playerChoice === computerChoice) {
+    return "It is a draw!";
+  } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+    return "You lose! Paper beats Rock!";
+  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+    return "You win! Rock beats Scissors!";
+  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+    return "You win! Paper beats Rock!";
+  } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+    return "You lose! Scissors beats Paper!";
+  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+    return "You win! Scissors beats Paper!";
+  } else {
+    return "You lose! Rock beats Scissors!";
+  }
+}
