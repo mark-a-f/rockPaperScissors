fb64b2956968b30b06dab6d589fef69e47342089
Mark Fisher <mafisher888@gmail.com>
2023-03-25 10:09:52 +0000

    Fix the IF block error within playRound()
    playerChoice parameter formatted incorrectly causing if block to not trigger any options

--- js/script.js
+++ js/script.js
@@ -13,21 +13,38 @@ function getComputerChoice() {
 function playRound(playerChoice, computerChoice) {
   // Lowercase playerChoice and capitalize first letter to match computerChoice format
   playerChoice = playerChoice.toLowerCase();
-  playerChoice = playerChoice.charAt(0).toUpperCase + playerChoice.slice(1);
 
-  if (playerChoice === computerChoice) {
+  let firstLetter = playerChoice.charAt(0);
+  let firstLetterCap = firstLetter.toUpperCase();
+  let remainingWord = playerChoice.slice(1);
+  let formattedPlayerChoice = firstLetterCap + remainingWord;
+
+  if (formattedPlayerChoice === computerChoice) {
     return "It is a draw!";
-  } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+  } else if (formattedPlayerChoice === "Rock" && computerChoice === "Paper") {
     return "You lose! Paper beats Rock!";
-  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+  } else if (
+    formattedPlayerChoice === "Rock" &&
+    computerChoice === "Scissors"
+  ) {
     return "You win! Rock beats Scissors!";
-  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+  } else if (formattedPlayerChoice === "Paper" && computerChoice === "Rock") {
     return "You win! Paper beats Rock!";
-  } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+  } else if (
+    formattedPlayerChoice === "Paper" &&
+    computerChoice === "Scissors"
+  ) {
     return "You lose! Scissors beats Paper!";
-  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+  } else if (
+    formattedPlayerChoice === "Scissors" &&
+    computerChoice === "Paper"
+  ) {
     return "You win! Scissors beats Paper!";
   } else {
     return "You lose! Rock beats Scissors!";
   }
 }
+
+const playerChoice = "Rock";
+const computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
